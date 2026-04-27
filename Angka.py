1e1dd7b827c7bb3a0951c31d61562397cdc112f7
zazkiapertiwi02-prog <zazkiapertiwi02@gmail.com>
2026-04-27 10:21:37 +0700

    Add files via upload

---
+++ Angka.py
@@ -0,0 +1,6 @@
+angka = float(input("Masukkan angka: "))
+
+while angka <= -0.1:
+    print("harus positif!!!")
+    angka = float(input("Masukkan angka: "))
+print(f"angka yang dimasukkan adalah: {angka}")
